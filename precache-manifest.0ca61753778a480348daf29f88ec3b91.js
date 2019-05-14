631009231758a59c9d6a9159d9d82cd337314645
avalos010 <luizavalos40@gmail.com>
2019-05-14 14:32:18 -0500

    Updates

---
+++ precache-manifest.0ca61753778a480348daf29f88ec3b91.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f13ebb969c2544da5c69089e8732855b",
+    "url": "/News-React-App/./index.html"
+  },
+  {
+    "revision": "b5712d276a516cc9b745",
+    "url": "/News-React-App/./static/css/main.254c58ab.chunk.css"
+  },
+  {
+    "revision": "8369a2bdba3e61158bb1",
+    "url": "/News-React-App/./static/js/2.679f4bcc.chunk.js"
+  },
+  {
+    "revision": "b5712d276a516cc9b745",
+    "url": "/News-React-App/./static/js/main.79e3eddf.chunk.js"
+  },
+  {
+    "revision": "2722d73ba5760495f40c",
+    "url": "/News-React-App/./static/js/runtime~main.59ba018d.js"
+  }
+]);
